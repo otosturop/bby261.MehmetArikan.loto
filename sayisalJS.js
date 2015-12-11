5852b19bd60ecb9c54635c24105646727bd51f4b
mehmet <mehmetarikan.ma@gmail.com>
2015-12-11 16:06:53 +0200

    Virgül düzeltme

--- sayisalJS.js
+++ sayisalJS.js
@@ -3,8 +3,8 @@ function Rasgele(min,max){
 }
 
 function yaz() {
-     var depo,yedek,depo2,yedek2,yedek3, ilk=0,ilk2=0,ilk3=0,ilk1=0,ilk4=0, 
-    sayilar=new Array(6),rakamlar=new Array(6),sayilar2=new Array(6),rakamlar2=new Array(6),
+    var depo,yedek,depo2,yedek2,yedek3,ilk=0,ilk2=0,ilk3=0,ilk1=0,ilk4=0;
+    var sayilar=new Array(6), rakamlar=new Array(6), sayilar2=new Array(6), rakamlar2=new Array(6);
     sayilar3=new Array(6);
     var kontrol=true;  
 for(var i=0;i<6;i++)
@@ -52,11 +52,13 @@ for(var i=0;i<6;i++)
       kontrol=true;
       for(i=0;i<6;i++){
         if(rakamlar[i]==yedek){
-                kontrol=false;}
+                kontrol=false;
+        }
       }
     if(kontrol==true){
         rakamlar[ilk2]=yedek;
-        ilk2++;  }  
+        ilk2++;
+    }  
   }
     
     for(var k=1;k<6;k++){
@@ -64,7 +66,8 @@ for(var i=0;i<6;i++)
         if(rakamlar[i]>rakamlar[i+1]){
 		yedek=rakamlar[i];
 		rakamlar[i]=rakamlar[i+1];
-		rakamlar[i+1]=yedek;}
+		rakamlar[i+1]=yedek;
+        }
    }
 }	
     //ikinci satır
@@ -81,7 +84,8 @@ for(var i=0;i<6;i++)
       }
     if(kontrol==true){
         rakamlar2[ilk1]=yedek2;
-        ilk1++;  }  
+        ilk1++;
+    }  
   }
     
     for(var k=1;k<6;k++){
@@ -89,26 +93,30 @@ for(var i=0;i<6;i++)
         if(rakamlar2[i]>rakamlar2[i+1]){
 		yedek2=rakamlar2[i];
 		rakamlar2[i]=rakamlar2[i+1];
-		rakamlar2[i+1]=yedek2;}
+		rakamlar2[i+1]=yedek2;
+        }
    }
 }	
     
     //üçüncü satır
     
     
-    	for(var i=0;i<6;i++)
-    {sayilar2[i]=0;}
+    	for(var i=0;i<6;i++){
+            sayilar2[i]=0;
+        }
     
   while(ilk3<6)
   { yedek3=Rasgele(1,49);
       kontrol=true;
       for(i=0;i<6;i++){
         if(sayilar2[i]==yedek3){
-                kontrol=false;}
+                kontrol=false;
+        }
       }
     if(kontrol==true){
         sayilar2[ilk3]=yedek3;
-        ilk3++;  }  
+        ilk3++;
+    }  
   }
     
     for(var k=1;k<6;k++){
@@ -116,24 +124,29 @@ for(var i=0;i<6;i++)
         if(sayilar2[i]>sayilar2[i+1]){
 		yedek3=sayilar2[i];
 		sayilar2[i]=sayilar2[i+1];
-		sayilar2[i+1]=yedek3;}
+		sayilar2[i+1]=yedek3;
+        }
    }
 }	
     
      //dördüncü satır
     for(var i=0;i<6;i++)
-    {sayilar3[i]=0;}
+    {
+        sayilar3[i]=0;
+    }
     
   while(ilk4<6)
   { depo2=Rasgele(1,49);
       kontrol=true;
       for(i=0;i<6;i++){
         if(sayilar3[i]==depo2){
-                kontrol=false;}
+                kontrol=false;
+        }
       }
     if(kontrol==true){
         sayilar3[ilk4]=depo2;
-        ilk4++;  }  
+        ilk4++;
+    }  
   }
     
     for(var k=1;k<6;k++){
@@ -141,7 +154,8 @@ for(var i=0;i<6;i++)
         if(sayilar3[i]>sayilar3[i+1]){
 		depo2=sayilar3[i];
 		sayilar3[i]=sayilar3[i+1];
-		sayilar3[i+1]=depo2;}
+		sayilar3[i+1]=depo2;
+        }
    }
 }	
 
